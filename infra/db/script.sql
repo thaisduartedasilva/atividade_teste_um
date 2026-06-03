95d43b1f241fffea08377fad4f0142460a06bc08
thaisduarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-06-03 08:12:42 -0300

    add - comentarios script.sql

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE sistema_simples_m1;
+CREATE DATABASE sistema_simples_m1; -- Cria o banco de dados
 
-USE sistema_simples_m1;
+USE sistema_simples_m1; -- usa o banco de dados criado
 
-CREATE TABLE usuarios (
+CREATE TABLE usuarios ( -- Cria a tabela de usuários
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(87) NOT NULL,
     senha VARCHAR(255) NOT NULL
 );
 
-INSERT INTO usuarios (usuario, senha) VALUES ('admin','123');
+INSERT INTO usuarios (usuario, senha) VALUES ('admin','123'); -- Insere um usuário e senha 
