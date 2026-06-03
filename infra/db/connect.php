8e94d3caedf510f5094563f53876dc2f830e40d1
thaisduarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-06-03 08:09:35 -0300

    add - comentarios connect

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,13 +1,13 @@
 <?php
 
-    $host = "localhost";
-    $user = "root";
-    $pass = "root";
-    $db = "sistema_simples_m1";
+    $host = "localhost"; // Endereço do servidor MySQL
+    $user = "root"; // Usuário do banco de dados 
+    $pass = "root"; // Senha do banco
+    $db = "sistema_simples_m1"; // Nome do banco de dados
 
-    $conn = new mysqli($host,$user,$pass,$db);
+    $conn = new mysqli($host,$user,$pass,$db); // Cria a conexão com o MySQL
 
-    if($conn->connect_error){
+    if($conn->connect_error){ // Verifica se ocorreu erro na conexão
         die("Erro na conexão!");
     }else{
         echo "<script>console.log('Banco conectado com sucesso!')</script>";
